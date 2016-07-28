563c5541cb5453f75534304ea70019445dcc83fe
nadaj <nadajankovic2004@yahoo.com>
2016-07-28 13:51:44 +0200

    VotingSuccess

--- app/Console/Commands/VotingSuccess.php
+++ app/Console/Commands/VotingSuccess.php
@@ -3,6 +3,9 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use App\Voting;
+use DB;
+use Carbon\Carbon;
 
 class VotingSuccess extends Command
 {
@@ -37,6 +40,59 @@ class VotingSuccess extends Command
      */
     public function handle()
     {
-        
+        // change statuses for finished votings
+        $date_now = Carbon::now();
+        $votings = Voting::where('to', '<', $date_now)
+                            ->where('status', '=', 0)
+                            ->get();
+        foreach ($votings as $voting) 
+        {
+            // get criterium and check if it's true
+            $voting_success = DB::table('voting_success')->where('voting_id', '=', $voting->id)
+                                                         ->get();
+            $voting_success = $voting_success[0];
+
+            $success = 2;   //fail
+            if ($voting_success->answer_id == null)     // number of people voted
+            {
+                $num_voted = DB::table('tickets')->join('answers_tickets', 't.id', '=', 'answers_tickets.tickets_id')
+                                    ->select('answers_tickets.tickets_id')
+                                    ->where('tickets.votings_id', '=', $voting->id)
+                                    ->distinct()
+                                    ->count();
+            }
+            else
+            {
+                $num_voted = DB::table('tickets')->join('answers_tickets', 'tickets.id', '=', 'answers_tickets.tickets_id')
+                                    ->select('answers_tickets.tickets_id')
+                                    ->where('tickets.votings_id', '=', $voting->id)
+                                    ->where('answers_tickets.answers_id', '=', $voting_success->answer_id)
+                                    ->distinct()
+                                    ->count();
+            }
+
+            switch ($voting_success->relation)
+            {
+                case "=":
+                    if ($num_voted == $voting_success->value)
+                        $success = 1; break;
+                case "<>":
+                    if ($num_voted != $voting_success->value)
+                        $success = 1; break;
+                case ">":
+                    if ($num_voted > $voting_success->value)
+                        $success = 1; break;
+                case "<":
+                    if ($num_voted < $voting_success->value)
+                        $success = 1; break;
+                case ">=":
+                    if ($num_voted >= $voting_success->value)
+                        $success = 1; break;
+                case "<=":
+                    if ($num_voted <= $voting_success->value)
+                        $success = 1; break;
+            }
+            Voting::where('id', '=', $voting->id)->update(['status' => $success]);
+        }
     }
 }
--- app/Voting.php
+++ app/Voting.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Voting extends Model
 {
+	public $timestamps = false;
     protected $fillable = ['name', 'description', 'from', 'to', 'multiple_answers','status', 'initiator_id'];
 }
